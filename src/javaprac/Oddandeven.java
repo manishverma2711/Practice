d49a232146f55c06aa90943dbfba850f94a92848
Manish Verma <manish2711.mv@gmail.com>
2023-10-19 15:47:56 +0530

    Even and Odd program

---
+++ src/javaprac/Oddandeven.java
@@ -0,0 +1,25 @@
+package javaprac;
+
+import java.util.Scanner;
+
+public class Oddandeven {
+public static void main(String[] args) {
+		
+		System.out.println("Enter a number: ");
+		
+		Scanner sc= new  Scanner(System.in);
+		int i= sc.nextInt();
+
+		
+		if(i%2==0) {
+			
+			System.out.println("Its even number :" + i);
+		}
+		else {
+			
+			System.out.println("Its odd number: " + i);
+		}
+		
+		
+	}
+}
