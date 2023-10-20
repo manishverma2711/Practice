900999027ceba918bae73e658bae07ea0cf57c75
Manish Verma <manish2711.mv@gmail.com>
2023-10-20 15:17:46 +0530

    Reverse String

---
+++ src/javaprac/Reversenumber.java
@@ -0,0 +1,17 @@
+package javaprac;
+
+public class Reversenumber {
+	
+	public static void main(String[] args) {
+		
+		String str = "TESTER";
+		
+		String ok;
+		
+		StringBuffer a = new StringBuffer(str);
+		System.out.println(a.reverse());
+		
+		
+	}
+
+}
