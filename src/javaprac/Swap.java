5c89967fd568426a384ecfd0a3618dfd3b369d66
Manish Verma <manish2711.mv@gmail.com>
2023-10-21 22:19:11 +0530

    Swap Number

---
+++ src/javaprac/Swap.java
@@ -0,0 +1,22 @@
+package javaprac;
+
+public class Swap {
+
+	public static void main(String[] args) {
+   
+		int a=10;
+		int b=20;
+		
+
+		a= b-a;	
+		
+		
+			
+		System.out.println("Print the value of a is :" + b );
+		
+		b= b-a;
+		System.out.println("Print the value of b is :" + b );
+
+	}
+
+}
