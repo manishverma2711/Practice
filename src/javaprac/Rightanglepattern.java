5464d899b00b226e09a3921f687f9bfac38e45b4
Manish Verma <manish2711.mv@gmail.com>
2023-11-01 15:58:43 +0530

    Rightangle Trinagle

---
+++ src/javaprac/Rightanglepattern.java
@@ -0,0 +1,24 @@
+package javaprac;
+
+public class Rightanglepattern {
+
+	public static void main(String[] args) {
+		
+		for(int i=1; i<5;i++)
+		{
+			for(int k=5;k>i;k--) {
+		
+				System.out.print(" ");
+				
+			}		
+			
+		    for(int j=0;j<i;j++)
+		    {
+		    	System.out.print(" *");
+		    }	
+		    
+		    System.out.println("  ");
+		}
+		
+	}
+}
