5ab570313be9e728515ae2e7ff824e515cdf5bd9
Manish Verma <manish2711.mv@gmail.com>
2023-11-21 14:28:39 +0530

    The loop

---
+++ src/javaprac/loop.java
@@ -0,0 +1,16 @@
+package javaprac;
+
+public class loop {
+
+	public static void main(String[] args) {
+		
+		
+		for(int a=1;a<=10;a++) {
+			
+			System.out.println("The value of a is " + a);
+			
+		}
+		
+		
+	}
+}
