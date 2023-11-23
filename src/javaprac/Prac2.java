8bcd3ac8d3790a7f8390ff136911f13640f45944
Manish Verma <manish2711.mv@gmail.com>
2023-11-23 11:28:13 +0530

    The length of the string

---
+++ src/javaprac/Prac2.java
@@ -0,0 +1,14 @@
+package javaprac;
+
+public class Prac2 {
+
+	public static void main(String[] args) {
+		
+		String len= "Find the Length";
+				
+		System.out.println("The lenth of the given string is "+len.length());
+		
+		
+	}
+
+}
