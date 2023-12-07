76bceed4cc963674df0dac9ebb5334afd8ce2131
Manish Verma <manish2711.mv@gmail.com>
2023-12-07 17:03:26 +0530

    Static method example

---
+++ src/javaprac/Staticmethod.java
@@ -0,0 +1,20 @@
+package javaprac;
+
+public class Staticmethod {
+	
+   public static void hey() {
+	
+	   System.out.println("How are you men! ");
+}
+
+	public static void main(String[] args) {
+		
+   
+     	hey(); // calling directly
+		
+		Staticmethod.hey();  // calling by through class name.
+
+		
+	}
+	
+}
