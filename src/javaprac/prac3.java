a2466bc34031b9314c1dfe444fa1e3d2d98b984c
Manish Verma <manish2711.mv@gmail.com>
2024-01-03 15:50:16 +0530

    Powerprogram

---
+++ src/javaprac/prac3.java
@@ -0,0 +1,22 @@
+package javaprac;
+
+public class prac3 {
+
+	
+	    public static void main(String[] args) {
+	 
+	        int number = 2, p = 10;
+	        long result = 1;
+	        
+	      
+	        int i = p;
+	        for (;i != 0; --i)
+	        {
+	            result *= number;
+	        }
+	        
+	     
+	        System.out.println(number+"^"+p+" = "+result);
+	    
+	}
+}
