c52eeab81e5c3374b29cba55b5e4822abf3c597d
Manish Verma <manish2711.mv@gmail.com>
2024-01-28 09:12:29 +0530

    Reverse string by taking input from user

--- src/javaprac/reversenooutputfromuser.java
+++ src/javaprac/reversenooutputfromuser.java
@@ -1,8 +1,28 @@
 package javaprac;
 
+import java.util.Scanner;
+
 public class reversenooutputfromuser {
 
-	String str =""
+	public static void main(String[] args) {
 	
+		 String str= "Geeks", nstr="";
+	        char ch;
+	       
+	      System.out.print("Original word: ");
+	      System.out.println("Geeks"); 
+	       
+	      for (int i=0; i<str.length(); i++)
+	      {
+	        ch= str.charAt(i); 
+	        nstr= ch+nstr; 
+	      }
+	      System.out.println("Reversed word: "+ nstr);
+		
+		
+		
+	}
+	
+//	String str ="Manish";
 	
 }
